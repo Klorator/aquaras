770ab212d36bd228c1fbbd6b6cb12db5699650af
Rasmus Hammar <Rasmus.Hammar@gmx.com>
2022-08-14 22:41:50 +0200

    Fixed dicumentation for Example_Runlist (turned off markdown support)

--- R/data.R
+++ R/data.R
@@ -2,24 +2,26 @@
 #'
 #' An example runlist for demonstration and test purposes.
 #'
+#' @noMd
+#'
 #' @format A data frame with 672 rows and 14 variables:
 #' \describe{
 #'   \item{Index}{Unique row identifier}
 #'   \item{Plate}{Well plate number, 3–9}
 #'   \item{Row}{Row letter, A-H}
 #'   \item{Col}{Column number, 1-12}
-#'   \item{LC_Position}{Well coordinate, composite of "`Plate`:`Row`,`Col`"}
+#'   \item{LC_Position}{Well coordinate, composite of "\code{Plate}:\code{Row},\code{Col}"}
 #'
 #'   \item{Date}{Date as character string}
 #'   \item{Signature}{Signature to identify owner}
-#'   \item{Sample_name}{Sample name, composite of "`Date`_`Signature`_Index.`Index`"}
+#'   \item{Sample_name}{Sample name, composite of "\code{Date}_\code{Signature}_Index.\code{Index}"}
 #'   \item{Compound}{Name of compound being tested}
 #'   \item{Timepoint}{Integer for plotting timeseries}
 #'
 #'   \item{Well_Type}{Type of well contents, one of "bead", "medium", "cell", "STD", or "blank"}
 #'   \item{LC_Well_Type}{Either "blank" or "Analyte"}
 #'   \item{Replicate}{Integer for numbering replicate experiments}
-#'   \item{Sample_text}{Sample text, composite of "`Compound`_`Timepoint`_`Well_Type`_`Replicate`"}
+#'   \item{Sample_text}{Sample text, composite of "\code{Compound}_\code{Timepoint}_\code{Well_Type}_\code{Replicate}"}
 #'  }
 #' @source Created in house
 "Example_Runlist"

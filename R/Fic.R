3cdb7f80e6045ae7cc7fee50ba89776fb14e942d
Rasmus Hammar <Rasmus.Hammar@gmx.com>
2023-09-13 22:32:50 +0200

    Fic -- extract buffer values

--- R/Fic.R
+++ R/Fic.R
@@ -1,5 +1,8 @@
 #' Cleaning for Fic calculation
 #'
+#' The Sample.Text needs to follow the pattern of Sample ID, Type of liquid,
+#' Timepoint, Replicate; separated by underscores. E.g. "Hec42_Medium_30_1".
+#'
 #' @param df A dataframe from the MassLynx complete summary output or similar
 #'
 #' @return Same dataframe but filtered and with Sample.Text separated into columns
@@ -21,4 +24,33 @@ ras.clean_for_Fic <- function(df) {
                                           too_few = "align_end",
                                           cols_remove = F)
   return(df_clean)
+}
+#' Extract the buffer concentration
+#'
+#' This feels too specific.
+#'
+#' @param df A dataframe from [ras.clean_for_Fic()].
+#' @param values Name of the column to use for values
+#' @param type String to filter the column `LiquidType` by
+#'
+#' @return Dataframe with columns `Sample_ID`, `LiquidType`, & `Buffer_Conc.avg`
+#' @noRd
+ras.Fic_buffer <- function(df, values = "Conc.", type = "HBSS") {
+  # Drop other columns and filter for HBSS
+  df_buffer <- df %>%
+    dplyr::select(Sample_ID,
+                  LiquidType,
+                  {{values}})
+  df_buffer <- df_buffer %>%
+    dplyr::filter(stringr::str_detect(LiquidType, {{type}}))
+  # Group by Sample_ID and average
+  df_buffer <- df_buffer %>%
+    dplyr::group_by(Sample_ID) %>%
+    dplyr::mutate(Buffer_Conc.avg = mean({{values}}),
+                  .keep = "unused")
+  # Remove duplicates to keep only one average value
+  df_buffer <- df_buffer %>%
+    dplyr::arrange(Sample_ID) %>%
+    dplyr::filter(duplicated(Sample_ID) == FALSE)
+  return(df_buffer)
 }
